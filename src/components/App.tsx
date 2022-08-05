8f506b1dde2ebdb189e2e1d02d17899f85250576
StepanKhachikyan1999 <[stepan-khachikyan@mail.ru]>
2022-08-05 17:48:35 +0400

    commit

---
+++ src/components/App.tsx
@@ -0,0 +1,65 @@
+import {ITodo} from '../types/data'
+import {TodoList} from "./TodoList";
+import {useEffect, useRef, useState} from "react";
+
+const App:React.FC = () => {
+
+    const [value,setValue] = useState('')
+    const [todos,setTodos] = useState<ITodo[]>([])
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const handleChange:React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        setValue(e.target.value)
+    }
+
+    const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+        if (e.key === 'Enter')  addTodo()
+    }
+
+    const addTodo = () => {
+        if(value) {
+            setTodos([...todos,{
+                id:Math.random(),
+                title:value,
+                complete:false
+            }])
+
+            setValue('')
+        }
+
+    }
+
+    const removeTodo = (id:number):void  => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    const toggleTodo = (id:number):void => {
+            setTodos(todos.map(todo => {
+                if(todo.id !== id) return todo
+
+                return {
+                    ...todo,
+                    complete: !todo.complete
+                }
+            }))
+    }
+
+
+    useEffect(() => {
+        if(inputRef.current) inputRef.current.focus()},[])
+
+
+    return (
+        <div>
+            <div>
+                <input value={value} onChange={handleChange} onKeyDown={handleKeyDown} ref={inputRef} type="text"/>
+                <button onClick={addTodo}>add</button>
+            </div>
+            <TodoList items={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
+        </div>
+    )
+}
+
+
+export {App}
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,32 @@
+import {TodoItem} from "./TodoItem";
+import {ITodo} from "../types/data";
+
+interface ITodoListProps{
+    items:ITodo[],
+    removeTodo: (id:number) => void
+    toggleTodo: (id:number) => void
+}
+
+const TodoList:React.FC<ITodoListProps> = (props) => {
+
+   const {items,removeTodo,toggleTodo} = props
+    return (
+        <div>
+            {/*{*/}
+            {/*    props.items.map(todo => <TodoItem key={todo.id} {...todo} />)*/}
+            {/*}*/}
+
+            {
+                items.map(todo => (
+                    <TodoItem
+                        key={todo.id}
+                        toggleTodo={toggleTodo}
+                        removeTodo={removeTodo}
+                        {...todo}/>
+                ))
+            }
+        </div>
+    )
+}
+
+export {TodoList}
